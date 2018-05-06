a5e9f7e45dc0ddaf56d85e1f5d556fc16507dc4f
Mary Xia <maryxia@google.com>
2018-05-06 14:27:13 -0700

    add const colors

--- io-talk/shrine/lib/colors.dart
+++ io-talk/shrine/lib/colors.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 // Light theme
 const kShrinePink = const Color(0xFFFEDBD0);
+const kShrinePinkSecondary = const Color(0xFFFEEAE6);
 const kShrineBrown = const Color(0xFF442B2D);
+const kShrineWhite = const Color(0xFFFFFBFA);
 
 // Alt theme
 const kShrineGreen100 = const Color(0xFF96f7aa);
